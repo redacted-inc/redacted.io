cf7637c295459d69fb9373a28b765c71c3e196c0
Eric Wright <dev@ericwright.me>
2012-01-30 18:07:18 -0500

    Initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedactedIo::Application.config.secret_token = '65e07ba91e500db871c7d17fa1a7fc23952d278e6033949b089093942ead22fdaf99f7e698076dd13510b70f7ee7b436593eaeddb2573dbd81bf11fb16715287'
